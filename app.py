96b6a4a7a3e0b262ce912094966b0d316caf2284
rivkih <rivki.csui@gmail.com>
2021-10-25 08:04:30 +0700

    update trips add route

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import sqlite3
-#import requests
 from tqdm import tqdm
 import json 
 import numpy as np
@@ -17,10 +16,39 @@ def get_all_stations(conn):
     result = pd.read_sql_query(query, conn)
     return result
 
+def get_station_id(station_id, conn):
+    query = f"""SELECT * FROM stations WHERE station_id = {station_id}"""
+    result = pd.read_sql_query(query, conn)
+    return result 
+
+def insert_into_stations(data, conn):
+    query = f"""INSERT INTO stations values {data}"""
+    try:
+        conn.execute(query)
+    except Exception as ex:
+        return ex.args
+    conn.commit()
+    return 'OK'
+
 def get_all_trips(conn):
     query = f"""SELECT * FROM trips"""
     result = pd.read_sql_query(query, conn)
     return result
+
+
+def get_trip_id(trip_id, conn):
+    query = f"""SELECT * FROM trips WHERE id={trip_id}"""
+    result = pd.read_sql_query(query, conn)
+    return result 
+
+def insert_into_trips(data, conn):
+    query = f""" INSERT INTO trips values {data}"""
+    try:
+        conn.execute(query)
+    except:
+        return 'Error'
+    conn.commit()
+    return 'OK'
 
 ##### route function #####
 
@@ -28,17 +56,60 @@ def get_all_trips(conn):
 def home():
     return 'Hello World'
 
+@app.route('/json',methods=['POST']) 
+def json_example():
+
+    req = request.get_json(force=True) # Parse the incoming json data as Dictionary
+
+    name = req['name']
+    age = req['age']
+    address = req['address']
+
+    return (f'''Hello {name}, your age is {age}, and your address in {address}
+            ''')
+
 @app.route('/stations/')
 def route_all_stations():
     conn = make_connection()
     stations = get_all_stations(conn)
     return stations.to_json()
 
+@app.route('/stations/<station_id>')
+def route_stations_id(station_id):
+    conn = make_connection()
+    station = get_station_id(station_id, conn)
+    return station.to_json()
+
+@app.route('/stations/add', methods=['POST']) 
+def route_add_station():
+    # parse and transform incoming data into a tuple as we need 
+    data = pd.Series(eval(request.get_json(force=True)))
+    data = tuple(data.fillna('').values)
+    conn = make_connection()
+    result = insert_into_stations(data, conn)
+    return result
+
 @app.route('/trips/')
 def route_all_trips():
     conn=make_connection()
     trips = get_all_trips(conn)
     return trips.to_json()
+
+@app.route('/trips/<trip_id>')
+def route_trips_id(trip_id):
+    conn = make_connection()
+    trip = get_trip_id(trip_id,conn)
+    return trip.to_json()
+
+@app.route('/trips/add', methods=['POST'])
+def route_add_trip():
+    # parse and transform incoming data into a tuple as we need 
+    data = pd.Series(eval(request.get_json(force=True)))
+    data = tuple(data.fillna('').values)
+    conn = make_connection()
+    result = insert_into_trips(data, conn)
+    return result
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
